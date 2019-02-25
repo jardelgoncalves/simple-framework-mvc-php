c1cd81aaad9eeef2298021d7c418540ea420a986
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-02-25 00:04:35 -0300

    add model Users com métodos de acesso ao banco de dados

---
+++ Application/models/Users.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Application\models;
+
+use Application\core\Database;
+use PDO;
+class Users
+{
+  /** Poderiamos ter atributos aqui */
+
+  /**
+  * Este método busca todos os usuários armazenados na base de dados
+  *
+  * @return   array
+  */
+  public static function findAll()
+  {
+    $conn = new Database();
+    $result = $conn->executeQuery('SELECT * FROM users');
+    return $result->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  /**
+  * Este método busca um usuário armazenados na base de dados com um
+  * determinado ID
+  * @param    int     $id   Identificador único do usuário
+  *
+  * @return   array
+  */
+  public static function findById(int $id)
+  {
+    $conn = new Database();
+    $result = $conn->executeQuery('SELECT * FROM users WHERE id = :ID LIMIT 1', array(
+      ':ID' => $id
+    ));
+
+    return $result->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+}
