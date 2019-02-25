cb54260dc0cc6f5a635306e36ac0e48f6b68f738
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-02-25 00:03:52 -0300

    add controller da home

---
+++ Application/controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+use Application\core\Controller;
+
+class Home extends Controller
+{
+  /*
+  * chama a view index.php do  /home   ou somente   /
+  */
+  public function index()
+  {
+    $this->view('home/index');
+  }
+
+}
